0f8ce0f233905e1bc5b96109565e576850cb70ea
Felippe Kulkamp Sant Ana <felippeks@gmail.com>
2023-05-10 22:14:41 -0300

    finalizando trabalho

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import './App.scss';
-import Navbar from './components/Navgation';
-import Hero from './components/Hero';
-import CenteredButton from './components/CenteredButton';
-import CreditCard from './components/CreditCard';
-import CardList from './components/CardList';
-import posts from './data/posts';
-import Institutional from './components/Institutional';
-import Fac from './components/Fac';
-import AccountModal from './components/AccountModal'
-import Footer from './components/Footer';
-import PecaModal from './components/SolicitacaoModal';
-import { useState } from 'react';
-
-
-
-
+import React from "react";
+import "./App.scss";
+import Navbar from "./components/Navgation";
+import Home from "./views/Home";
+import Footer from "./components/Footer";
+import AccountModal from "./components/AccountModal";
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./views/Login";
+import Dashboard from "./views/Dashboard";
 
 function App() {
   const [ShowModal, SetShowModal] = useState(false);
+  const [name, setName] = useState();
+  const [account, setAccount] = useState();
+  const isLogged = name && account;
+  const fakeAuth = {
+    login: (name, account, cb) => {
+      setName(name);
+      setAccount(account);
+      setTimeout(cb, 100);
+    },
+    logout: (cb) => {
+      setName();
+      setAccount();
+      setTimeout(cb, 100);
+    },
+  };
 
-  const [SolicitaModal, setSolicitaModal] = useState(false);
-
+  const PrivateRoute = ({ Component, logged}) => {
+    return logged ? <Component name={name} account={account} /> : <Navigate to='/login' />
+  }
   return (
     <div className="App">
-      <Navbar handleCreateAccount={() => SetShowModal(true)}/>
-      <Hero handleCreateAccount={() => SetShowModal(true)}/>
-      <CreditCard/>
-      <CardList posts={posts} handCreateAccount={() => setSolicitaModal(true)}/>
-      <PecaModal show={SolicitaModal} handClose={() => setSolicitaModal(false)}/>
-      <CenteredButton handleCreateAccount={() => SetShowModal(true)}/>
-      <Institutional handleCreateAccount={() => SetShowModal(true)}/>
-      <Fac/>
-      <AccountModal show={ShowModal} handleClose={() => SetShowModal(false)}/>
-      <Footer/>
+      <Navbar
+        handleCreateAccount={() => SetShowModal(true)} logged={isLogged} auth={fakeAuth}/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login auth={fakeAuth} />}></Route>
+        <Route path='/dashboard/*' element={<PrivateRoute Component={Dashboard} logged={isLogged} />} />
+
+      </Routes>
+      <AccountModal show={ShowModal} handleClose={() => SetShowModal(false)} />
+      <Footer />
     </div>
   );
 }
--- src/components/Navgation.js
+++ src/components/Navgation.js
@@ -1,49 +1,91 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import './Navgation.scss';
+import { Link, useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import "./Navgation.scss";
+import logo from "../assests/logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Navgation = ({ handleCreateAccount, logged, auth }) => {
+  const nagivate = useNavigate();
+  const handleClick = () => {
+    auth.logout(nagivate("/"));
+  };
 
 
-import logo from '../assests/logo.svg';
-
-const Navgation = ({ handleCreateAccount }) => {
   return (
-    <Navbar  expand="lg">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#">
-          <img src={logo} height={30} className='d-inline-block align-top' alt='Magbank logo' />
+        <Navbar.Brand as={Link} to={"/"}>
+          <img
+            src={logo}
+            height={30}
+            className="d-inline-block align-top"
+            alt="Magbank logo"
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll
-          >
-            <Nav.Link href="#Cartão">Cartão</Nav.Link>
-            <Nav.Link href="#Quem somos">Quem somos</Nav.Link>
-            <Nav.Link href="#Faq">FAQ</Nav.Link>
-          </Nav>
-          <ButtonGroup>
-            <Button variant="outline-light">
-              <NavDropdown title="Acessar a minha conta" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action4">
-                  Pessoa Fisica
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action5">
-                  Pessoa Juridica
-                </NavDropdown.Item>
-              </NavDropdown>
+
+        {logged && (
+          <>
+            <Form>
+              <div className="navbar__search-group d-none d-lg-flex">
+                <Form.Control type="text" placeholder="O que você procura?" />
+                <Button variant="link">
+                  <FontAwesomeIcon icon={faSearch} color="#FFF" />
+                </Button>
+              </div>
+            </Form>
+            <Button variant="outline-light px-4" onClick={handleClick}>
+              Sair
             </Button>
-            <Button variant="outline-light" onClick={() => handleCreateAccount()}>
-              Abra sua conta
-            </Button>
-          </ButtonGroup>
-        </Navbar.Collapse>
+          </>
+        )}
+
+        {!logged && (
+          <>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#Cartão">Cartão</Nav.Link>
+                <Nav.Link href="#Quem somos">Quem somos</Nav.Link>
+                <Nav.Link href="#Faq">FAQ</Nav.Link>
+              </Nav>
+              <ButtonGroup>
+                <Button variant="outline-light">
+                  <NavDropdown
+                    title="Acessar a minha conta"
+                    id="navbarScrollingDropdown"
+                  >
+                    <NavDropdown.Item as={Link} to={"/login"}>
+                      Pessoa Fisica
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={"/login"}>
+                      Pessoa Juridica
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Button>
+                <Button
+                  variant="outline-light"
+                  onClick={() => handleCreateAccount()}
+                >
+                  Abra sua conta
+                </Button>
+              </ButtonGroup>
+            </Navbar.Collapse>
+          </>
+        )}
       </Container>
     </Navbar>
   );
-}
+};
 
 export default Navgation;
